ea7e2edf2124230922cd69f0fa4552ec1faf914a
MatheGenie27 <78275290+MatheGenie27@users.noreply.github.com>
2024-03-20 16:30:15 +0100

    Storage Function added

--- Javascript/database.js
+++ Javascript/database.js
@@ -1,1 +1,2 @@
-const TOKEN = "pEi5FKcwpcQR2iv30dQpE9dkrGpKeizYWMbvw5JVF4vmEfGoRr6pXJxUgCtmfHIs";
+const STORAGE_TOKEN = "pEi5FKcwpcQR2iv30dQpE9dkrGpKeizYWMbvw5JVF4vmEfGoRr6pXJxUgCtmfHIs";
+const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
---
+++ Javascript/storage.js
@@ -0,0 +1,11 @@
+
+async function setItem(key, value) {
+    const payload = { key, value, token: STORAGE_TOKEN };
+    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload)})
+    .then(res => res.json());
+}
+
+async function getItem(key) {
+    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+    return fetch(url).then(res => res.json());
+}
