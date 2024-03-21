9f84fcde39170173773eba96c7759f9637cf2c7b
MatheGenie27 <78275290+MatheGenie27@users.noreply.github.com>
2024-03-21 20:32:36 +0100

    JSON Structure in database.js

--- Javascript/database.js
+++ Javascript/database.js
@@ -1,2 +1,55 @@
 const STORAGE_TOKEN = "pEi5FKcwpcQR2iv30dQpE9dkrGpKeizYWMbvw5JVF4vmEfGoRr6pXJxUgCtmfHIs";
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
+
+
+let tasks=[];
+let actualTask;
+
+let contacts[];
+let actualContact;
+
+let users=[];
+let actualUser;
+
+
+//Structure of JSON 
+
+
+//Task
+let task = {
+    'taskID': {Number},
+    'title': {String},
+    'description': {String},
+    'assignedTo': [
+                {String}, {String}
+    ],
+    'dueDate': {Date},
+    'priority': {String},
+    'category': {String},
+    'subtasks':[
+                {String}, {String} 
+    ]
+
+}
+
+//Contacts
+
+let contact = {
+    'contactID': {Number},
+    'name': {String},
+    'email': {String},
+    'phone': {String},
+    'initials':{String}
+
+}
+
+
+//User
+
+let user = {
+    'userID': {Number},
+    'email': {String},
+    'password': {String},
+    'name': {String}
+
+}
