7336d509b5e6534c587645fe8c88c7fdb87b4c2e
MarcelArndt <marcel.arndt@hotmail.com>
2024-03-24 19:16:07 +0100

    ich bin dum dum

--- Javascript/board.js
+++ Javascript/board.js
@@ -4,6 +4,20 @@ let awaitFeedback = [];
 let isDone = [];
 let list =[toDo,inProgress,awaitFeedback,isDone];
 
+let taskObjects = [
+    {   taskID: "23423412123asdd",
+        title:"Playground",
+        description:"muss gebaut werden",
+        assignedTo:"Peter",
+        dueDate:"nicht mehr heute",
+        priority:"hight",
+        category:"Task-Force-One",
+        currentProgress:0,
+        subtasks: [1,3]
+    },
+    
+]
+
 let dragZoneShow = false;
 
 function init_board() {
@@ -11,7 +25,18 @@ function init_board() {
     checkForCard();
     showNoCard();
     loadTasks();
+    pullTask();
     //createTask(title, description, assignedTo, dueDate, priority, category, subtasks);
+}
+
+
+function pullTask(){
+    toDo = [];
+    inProgress = [];
+    awaitFeedback = [];
+    isDone = [];
+    for (let i = 0; i < task.length; i++)
+        console.log(task[i]["currentProgress"])
 }
 
 
@@ -58,4 +83,33 @@ function showNoCard(){
 function startDrag(){
     show_dragzone();
     addEventListener("dragend", (event) => {remove_dragzone();})
+}
+
+
+function templateCard(id){
+    return `<div class="card" draggable="true" ondragstart="startDrag()" id="${'id'}">
+    <div class="category"><div class="tag blue">User Story</div><div class="tag yellow">Up-Side_Down</div><div class="tag turquoise">Beat-Saber</div></div>
+    <div class="headline">Kaffee sollte verboten werden</div>
+    <div class="content">Kaffee gilt schon seit Uhrzeiten als die Volksdroge schlicht hin...</div>
+    <div class="subtask-bar">    
+      <div class="bar">
+        <div class="progress-bar" style="width:66%;"></div>
+      </div>
+      200/300 Subtasks
+    </div>
+    <div class="footer-of-card">
+      <div class="submit-user-area">
+          <div class="avatar orange">OR</div>
+          <div class="avatar pruple">NP</div>
+          <div class="avatar yellow">YE</div>
+          <div class="avatar pink">PI</div>
+          <div class="avatar green">GR</div>
+          <div class="avatar turquoise">TU</div>
+          <div class="avatar red">HF</div>
+      </div>
+      <div class="priority">
+        <img src="../img/icons/priority-medium.svg">
+      </div>
+    </div>
+</div>`;
 }
