8319ef8173ecd04370cb0ae8114ec6826cc337d5
MatheGenie27 <78275290+MatheGenie27@users.noreply.github.com>
2024-03-24 17:07:32 +0100

    Prio buttons jetzt auch in der Funktionalität hübsch mit farbig passenden Bildumschaltungen

--- Javascript/addTaskPage.js
+++ Javascript/addTaskPage.js
@@ -77,19 +77,30 @@ function checkPrio(prio){
     let urgentButton = document.getElementById('urgentButton');
     let mediumButton = document.getElementById('mediumButton');
     let lowButton = document.getElementById('lowButton');
+    let urgentImage = document.getElementById('urgentButtonImage');
+    let mediumImage = document.getElementById('mediumButtonImage');
+    let lowImage = document.getElementById('lowButtonImage');
+
    
     switch(prio){
         case "urgent":
             if(priority=="none"){
                 urgentButton.classList.add('urgentButtonPressed');
+                urgentImage.src = "../img/icons/urgent-icon-white.svg";
+
                 priority = "urgent";
             } else if(priority == "urgent"){
                 urgentButton.classList.remove('urgentButtonPressed');
+                urgentImage.src = "../img/icons/urgent-icon.svg";
                 priority = "none";
             } else if (priority!="none"){
                 mediumButton.classList.remove('mediumButtonPressed');
+                mediumImage.src = "../img/icons/priority-medium.svg";
                 lowButton.classList.remove('lowButtonPressed');
+                lowImage.src= "../img/icons/low-icon.svg";
                 urgentButton.classList.add('urgentButtonPressed');
+                urgentImage.src = "../img/icons/urgent-icon-white.svg";
+
                 priority = "urgent";
             }
             break;
@@ -98,14 +109,19 @@ function checkPrio(prio){
         case "medium":
             if(priority=="none"){
                 mediumButton.classList.add('mediumButtonPressed');
+                mediumImage.src="../img/icons/priority-medium-white.svg";
                 priority = "medium";
             } else if (priority == "medium") {
                 mediumButton.classList.remove('mediumButtonPressed');
+                mediumImage.src="../img/icons/priority-medium.svg"
                 priority = "none";
             }else if (priority!="none"){
                 mediumButton.classList.add('mediumButtonPressed');
+                mediumImage.src="../img/icons/priority-medium-white.svg";
                 lowButton.classList.remove('lowButtonPressed');
+                lowImage.src= "../img/icons/low-icon.svg";
                 urgentButton.classList.remove('urgentButtonPressed');
+                urgentImage.src = "../img/icons/urgent-icon.svg";
                 priority = "medium";
             } 
             break;
@@ -113,14 +129,19 @@ function checkPrio(prio){
         case "low":
             if(priority=="none"){
                 lowButton.classList.add('lowButtonPressed');
+                lowButtonImage.src="../img/icons/low-icon-white.svg";
                 priority = "low";
             } else if (priority == "low") {
                 lowButton.classList.remove('lowButtonPressed');
+                lowButtonImage.src="../img/icons/low-icon.svg";
                 priority = "none";
             }else if (priority!="none"){
                 mediumButton.classList.remove('mediumButtonPressed');
+                mediumImage.src="../img/icons/priority-medium.svg"
                 lowButton.classList.add('lowButtonPressed');
+                lowButtonImage.src="../img/icons/low-icon-white.svg";
                 urgentButton.classList.remove('urgentButtonPressed');
+                urgentImage.src = "../img/icons/urgent-icon.svg";
                 priority = "low";
             } 
             break;
