9e0f667b8bbbb4c8d3119ef4e40ca526bdfd9c07
phischa <ph.git@mailbox.org>
2024-03-27 19:53:02 +0100

    first signUp.js

--- Javascript/signUp.js
+++ Javascript/signUp.js
@@ -2,10 +2,10 @@ const STORAGE_TOKEN = "pEi5FKcwpcQR2iv30dQpE9dkrGpKeizYWMbvw5JVF4vmEfGoRr6pXJxUg
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 
 const form = document.getElementById('form');
+const nameInput = document.getElementById('name');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const loginBtn = document.getElementById('login-btn');
-const guestBtn = document.getElementById('guest-btn');
+const confrim = document.getElementById('confirm');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -15,8 +15,8 @@ form.addEventListener('submit', (e) => {
 
 function checkInput() {
     form.value
+    nameInput.value
     email.value
-    password.value
-    loginBtn.value 
-    guestBtn.value
+    password.value 
+    confirm.value
 }
