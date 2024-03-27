eb9e20e41bd93f16196f18a08f9470fdc72caa06
phischa <ph.git@mailbox.org>
2024-03-27 19:36:18 +0100

    first start.js

---
+++ Javascript/start.js
@@ -0,0 +1,8 @@
+const STORAGE_TOKEN = "pEi5FKcwpcQR2iv30dQpE9dkrGpKeizYWMbvw5JVF4vmEfGoRr6pXJxUgCtmfHIs";
+const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
+
+const form = document.getElementById('email-login');
+const emailLogin = document.getElementById('email-login');
+const passwordLogin = document.getElementById('password-login');
+const loginBtn = document.getElementById('login-btn');
+const guestBtn = document.getElementById('guest-btn');
