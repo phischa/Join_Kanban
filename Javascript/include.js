00c68be994d1c3380734e8e839630a8b94d1ec0f
MarcelArndt <marcel.arndt@hotmail.com>
2024-03-29 08:16:28 +0100

    addet include.js

---
+++ Javascript/include.js
@@ -0,0 +1,22 @@
+async function includeHTML() {
+    console.log("includeHTML aufgerufen");
+    let includeElements = document.querySelectorAll("[w3-include-html]");
+    for (let i = 0; i < includeElements.length; i++) {
+      const element = includeElements[i];
+      file = element.getAttribute("w3-include-html"); // "includes/header.html"
+      let resp = await fetch(file);
+      if (resp.ok) {
+        element.innerHTML = await resp.text();
+      } else {
+        element.innerHTML = "Page not found";
+      }
+    }
+    await searchAndHideElements();
+}
+
+async function searchAndHideElements(){
+  let elements = document.querySelectorAll("[toHideElement]")
+  for (let i =0; i < elements.length; i++){
+    elements[i].classList.add("hideElement");
+  }
+}
