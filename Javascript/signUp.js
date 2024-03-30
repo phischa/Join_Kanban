fbb782b139b63ca15ebf97e7d08a1f248960fdc7
phischa <ph.git@mailbox.org>
2024-03-30 22:43:26 +0100

    js form validation

--- Javascript/signUp.js
+++ Javascript/signUp.js
@@ -1,26 +1,16 @@
 const form = document.getElementById('form');
-const name = document.getElementById('name');
+const username = document.getElementById('name');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const confrim = document.getElementById('confirm');
 
-let users = [
+/* let users = [
     {   userID: "1",
         email: "sofiam@gmail.com",
         password: "mypassword123",
         name: "Sofia Müller"
-    },
-    {   userID: "2",
-        email: "sofiam@gmail.com",
-        password: "mypassword123",
-        name: "Sofia Müller"
-    },
-    {   userID: "3",
-        email: "sofiam@gmail.com",
-        password: "mypassword123",
-        name: "Sofia Müller"
     }
-]
+] */
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -28,25 +18,50 @@ form.addEventListener('submit', (e) => {
     checkInput();
 });
 
-function checkInput() {
-    const formValue = form.value.trim(); 
-    const nameValue = name.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim(); 
-    const confirmValue = confirm.value.trim();
+function checkNameInput() {
+    const nameValue = username.value.trim();
 
     if(nameValue === '') {
-        // show error
-        // add error class
-        showErrorFor(name, 'Name darf nicht frei bleiben');
+        showErrorFor(username, 'Name darf nicht frei bleiben');
     } else {
-        showSuccessFor(name);
+        showSuccessFor(username);
+    }
+}
+
+function checkEmailInput() {
+    const emailValue = email.value.trim();
+
+    if(emailValue === '') {
+        showErrorFor(email, 'Email darf nicht frei bleiben');
+    } else if(!isEmail(emailValue)){
+        showErrorFor(email, 'Keine valide Emailadresse');
+
+        showSuccessFor(email);
+    }
+}   
+
+function checkPasswordInput() {
+    const passwordValue = password.value.trim(); 
+    
+    if(passwordValue === '') {
+        showErrorFor(password, 'Passwort darf nicht frei bleiben');
+    } else {
+        showSuccessFor(password);
+    }
+}   
+
+function checkConfirmInput() {
+    const confirmValue = confirm.value.trim();
+    if(confirmValue === '') {
+        showErrorFor(confirm, 'Passwort-Bestätigung darf nicht frei bleiben');
+    } else {
+        showSuccessFor(confirm);
     }
 }
 
 function showErrorFor(input, message) {
     let formControl = input.perentElement;
-    let  inputInfo = formControl.querySelector('input-info');
+    let inputInfo = formControl.querySelector('input-info');
 
     inputInfo.innerText = message;
 
@@ -54,6 +69,12 @@ function showErrorFor(input, message) {
 }
 
 showSuccessFor(input)
+
+
+function isEmail(email) {
+    const res = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return res.test(String(email).toLowerCase());
+}
 
 
 // Pseudo Funktion, die verhindert, dass der Signup button klickbar ist, bevor alles ausgefüllt ist
