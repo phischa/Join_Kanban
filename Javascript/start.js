7d547ecae066a27cad045cbe038159038cfe536c
phischa <ph.git@mailbox.org>
2024-04-02 16:43:22 +0200

    actualUser

--- Javascript/start.js
+++ Javascript/start.js
@@ -1,27 +1,16 @@
-/* let user = {
-    userID: {String},
-    email: {String},
-    password: {String},
-    name: {String}
-
-} */
-
 async function initLogin() {
     loadUsers();
     loadRememberMe();
 }
 
-const form = document.getElementById('form');
-const loginBtn = document.getElementById('login-btn');
-const guestBtn = document.getElementById('guest-btn');
-
 function login() {
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
-    let user = users.find(u => u.email == email && u.password == password);
+    user = users.find(u => u.email == email && u.password == password);
     rememberMe(email, password);
     if(user) {
         console.log('User gefunden')
+        actualUser = user;
         window.location.href = 'summary.html';
     } else {
         console.log('Kein User gefunden')
@@ -29,7 +18,7 @@ function login() {
 
 }
 
-function rememberMe(email, password) {
+function rememberMe(email) {
     let rememberMe = document.getElementById('remember-me').checked;
     if (rememberMe) {
       localStorage.setItem('rememberedEmail', email);
