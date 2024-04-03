abdfc450645543e50415907c3f1ceff81b72419b
phischa <ph.git@mailbox.org>
2024-04-03 18:05:19 +0200

    checkbox change try

--- Javascript/start.js
+++ Javascript/start.js
@@ -7,22 +7,12 @@ function login() {
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
     user = users.find(u => u.email == email && u.password == password);
-    rememberMe(email, password);
-    if(user) {
+    if (user) {
         actualUser = user;
         storeActualUser();
         window.location.href = 'summary.html';
     } else {
         console.log('Kein User gefunden')
-    }
-}
-
-function rememberMe(email) {
-    let rememberMe = document.getElementById('remember-me').checked;
-    if (rememberMe) {
-        localStorage.setItem('rememberedEmail', email);
-    } else {
-        localStorage.removeItem('rememberedEmail');
     }
 }
 
@@ -32,3 +22,19 @@ function loadRememberMe() {
         document.getElementById("email").value = rememberedEmail;
     }
 }
+
+function rememberMe() {
+    let checkbox = document.getElementById('remember-me');
+    email = document.getElementById('email').value;
+    
+    if (checkbox.src = '../img/icons/checkbox-default.svg') {
+        checkbox.src = '../img/icons/checkbox-checked.svg';
+        localStorage.setItem('rememberedEmail', email);
+
+    } else {
+        checkbox.src = '../img/icons/checkbox-default.svg';
+        localStorage.removeItem('rememberedEmail');
+    }
+}
+
+
