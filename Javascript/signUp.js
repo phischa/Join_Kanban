08b3944bf8249dfe1a2aa18191c687d9938cea8e
phischa <ph.git@mailbox.org>
2024-04-08 13:30:04 +0200

    slight change on database.js/user function

--- Javascript/signUp.js
+++ Javascript/signUp.js
@@ -80,33 +80,3 @@ function changeIconToVisibilityOff1() {
 function changeIconToVisibilityOff2() {
   document.getElementById('password-icon2').src = '../img/icons/visibility_off.svg';
 }
-
-
-
-
-
-
-
-// Pseudo Funktion, die verhindert, dass der Signup button klickbar ist, bevor alles ausgefüllt ist
-/* https://stackoverflow.com/questions/71805724/disabling-button-until-condition-is-met-javascript
-function btnDisabled() {
-    let btn = document.querySelector('signup-btn')
-
-let cash = 0;
-
-if (cash == 1) {
-  btn.disabled = false;
-} else {
-  btn.disabled = true;
-}
-}
-
-
-let myBtn = document.getElementById("myBtn");
-
-if (some condition) {
-    myBtn.disabled = true;
-} else {
-  myBtn.disabled = false;
-}
- */
