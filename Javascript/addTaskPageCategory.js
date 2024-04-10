0e38ade47589d44ad560281a155b2f6560d243bc
MatheGenie27 <78275290+MatheGenie27@users.noreply.github.com>
2024-04-10 12:58:08 +0200

    addTaskPageCategory.js has now comments

--- Javascript/addTaskPageCategory.js
+++ Javascript/addTaskPageCategory.js
@@ -1,14 +1,24 @@
 //----------- UI Handling --------------
 
+/**
+ * sets the optical focus on the artificial cateogry field
+ */
 function focusCategory() {
   document.getElementById("lcategoryname").style.border = "2px solid #25C0D4";
 }
 
+/**
+ * removes the optical focus of the artificial categroy field
+ */
 function blurCategory() {
   document.getElementById("lcategoryname").style.border =
     "0.063rem solid #D1D1D1";
 }
 
+/**
+ * changes the image of the arrow in the category select field depending on 
+ * whether the selectfield is expanded or not.
+ */
 function changeSelectArrow() {
   let select = document.getElementById("lcategoryname");
   let arrowImage;
@@ -24,6 +34,12 @@ function changeSelectArrow() {
   select.style.backgroundImage = "url('..//img/icons/" + arrowImage + "')";
 }
 
+/**
+ * Function gets an event (mouseclick on addTaskpage) and 
+ * closes the selectmenu with the according arrowchange, when the
+ * click was outside of the select menu. 
+ * @param {targetElement} targetElement - a mouseclick on the addTaskPage  
+ */
 function checkCategoryEventArea(targetElement) {
   let selectCategory = document.getElementById("lcategoryname");
 
