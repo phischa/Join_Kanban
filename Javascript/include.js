f4e0878e319a19aa60c74fa08e0e7c18c8962c02
phischa <ph.git@mailbox.org>
2024-04-15 21:09:17 +0200

    try to hide sidebar

--- Javascript/include.js
+++ Javascript/include.js
@@ -1,41 +1,43 @@
 async function includeHTML() {
-    let includeElements = document.querySelectorAll("[w3-include-html]");
-    for (let i = 0; i < includeElements.length; i++) {
-      const element = includeElements[i];
-      file = element.getAttribute("w3-include-html"); // "includes/header.html"
-      let resp = await fetch(file);
-      if (resp.ok) {
-        element.innerHTML = await resp.text();
-      } else {
-        element.innerHTML = "Page not found";
-      }
+  let includeElements = document.querySelectorAll("[w3-include-html]");
+  for (let i = 0; i < includeElements.length; i++) {
+    const element = includeElements[i];
+    file = element.getAttribute("w3-include-html"); // "includes/header.html"
+    let resp = await fetch(file);
+    if (resp.ok) {
+      element.innerHTML = await resp.text();
+    } else {
+      element.innerHTML = "Page not found";
     }
-    await searchAndHideElements();
+  }
+  await searchAndHideElements();
 }
 
 
-async function searchAndHideElements(){
-  let isToHide =  document.querySelectorAll("[isToHide]")
-  if (isToHide.length > 0){
-  let elements = document.querySelectorAll("[toHideElement]")
-    for (let i =0; i < elements.length; i++){
-        elements[i].classList.add("hideElement");
-        }
+async function searchAndHideElements() {
+  let isToHide = document.querySelectorAll("[isToHide]")
+  if (isToHide.length > 0) {
+    let elements = document.querySelectorAll("[toHideElement]")
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].classList.add("hideElement");
     }
+  }
 }
 
 
-function openNavbar(){
+function openNavbar() {
   document.getElementById('navbar').classList.remove('d-none');
 }
 
 
-function closeNavbar(){
+function closeNavbar() {
   document.getElementById('navbar').classList.add('d-none');
 }
 
-function addInitialsToHeader(actualUser){
-  console.log(actualUser);
-  let insert = document.getElementById('initialname');
-  insert.innerHTML = `${actualUser}`; 
+function sidebarRestricted(actualUser) {
+  if (actualUser) {
+    document.getElementById('navi-sidebar').classList.remove('d-none');
+  } else {
+    document.getElementById('navi-sidebar').classList.add('d-none');
+  }
 }
