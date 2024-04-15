77402f8fb3628d9cc5111579f6e9ce7f3e9b8576
MarcelArndt <marcel.arndt@hotmail.com>
2024-04-15 20:51:53 +0200

    fixed some bug at add-page on board

--- Javascript/board-addTaskPage.js
+++ Javascript/board-addTaskPage.js
@@ -1,24 +1,32 @@
 let currentColumn = 0;
 
-function openAddTask(){
+function openAddTask(number){
     addTaskInBoardInit();
+    currentColumn = number;
 }
 
 
 async function addTaskInBoardInit(setColumn = 0){
+    filteredContacts = [];
+    contactsOfAddPage = [];
+    assignedContacts = [];
+    filteredContacts = [];
+    currentColumn = setColumn;
+    renderAddTaskTemplateLightBox();
+    clearAddTask()
     setMinDate();
     loadTasks();
     loadUsers();
     await loadContacts();
     await loadActualUser();
-    pressMediumButton();
     addContactsToPage();
     CheckforUnclosedWindows();
     checkRequirementsMouseover();
     CheckMouseoutCreateTask();
     stopSubtaskPropagation();
     stopPropagationContacts();
-    currentColumn = setColumn;
+    
+    
 }
 
 
@@ -27,4 +35,32 @@ async function searchAndResortTask(TaskId, newColumn){
    list[coordinates[0]][coordinates[1]]["currentProgress"] = newColumn;
    await saveCurrentTask(null, null, TaskId);
    refreshColumnRender();
+}
+
+function renderAddTaskTemplateLightBox(){
+    content = document.getElementById("cardLightboxContent");
+    content.innerHTML = templateAddTaskLightbox();
+}
+
+function submitTaskOnBoard(){
+        let title = document.getElementById("ltitlename");
+        let description = document.getElementById("ldescriptionname").value;
+        let assigned = assignedContacts;
+        let date = document.getElementById("ldatename").value;
+        let prio = priority;
+        let category = document.getElementById("lcategoryname").value;
+        finalizeSubtasks();
+        let subtasks = finalSubtasksOfAddPage;
+        createTask(title.value, description, assigned, date, prio, category, subtasks);
+        storeTasks();
+        clearRenderArea();
+        title.value = title.defaultValue;
+        clearForm();
+        setTimeout(hideBlackbox(), 500);
+}
+
+function clearAddTask(){
+    let checkboxes = document.getElementById("checkboxes");
+    checkboxes.innerHTML = "";
+    clearForm();
 }
